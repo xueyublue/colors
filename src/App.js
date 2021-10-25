7c3c610fd682ba47eeff46a77595f2699a9ef81d
Xue Yu <xueyu@Xues-MBP.home.xue.sg>
2021-10-25 21:21:53 +0800

    added unstyled slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
     <div>
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
--- src/Palette.jsx
+++ src/Palette.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Slider from "rc-slider";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
+import "rc-slider/assets/index.css";
 
-function Palette({ colors }) {
-  const colorBoxes = colors.map((item) => <ColorBox background={item.color} name={item.name} key={item.name} />);
+function Palette({ palette }) {
+  const [level, setLevel] = useState(500);
+
+  const colorBoxes = palette.colors[level].map((item) => (
+    <ColorBox background={item.hex} name={item.name} key={item.name} />
+  ));
+
   return (
     <div className="Palette">
+      <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={(newLevel) => setLevel(newLevel)} />
       <div className="Palette-colors">{colorBoxes}</div>
     </div>
   );
