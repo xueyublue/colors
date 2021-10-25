73280da638a44d8952bc968825d80ee87ead3e3e
Xue Yu <xueyublue@gmail.com>
2021-10-25 16:52:59 +0800

    created react app, added palette

---
+++ src/Palette.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Palette() {
+  return (
+    <div className="Palette">
+      <div className="Palette-colors">PALETTE</div>
+    </div>
+  );
+}
+
+export default Palette;
