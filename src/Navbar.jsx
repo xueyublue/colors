74a54ca3e228a66809b1053f428233579ecf473f
Xue Yu <xueyublue@gmail.com>
2021-10-26 09:51:01 +0800

    added format selector

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
+import { MenuItem, Select } from "@material-ui/core";
 
-function Navbar({ level, handleChange }) {
+function Navbar({ level, onLevelChange, format, onFormatChange }) {
   return (
     <header className="Navbar">
       <div className="logo">
@@ -12,8 +13,15 @@ function Navbar({ level, handleChange }) {
       <div className="slider-container">
         <span>Level: {level}</span>
         <div className="slider">
-          <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={handleChange} />
+          <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={onLevelChange} />
         </div>
+      </div>
+      <div className="select-container">
+        <Select value={format} onChange={onFormatChange}>
+          <MenuItem value="hex">HEX - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+        </Select>
       </div>
     </header>
   );
--- src/Palette.jsx
+++ src/Palette.jsx
@@ -5,14 +5,22 @@ import "./Palette.css";
 
 function Palette({ palette }) {
   const [level, setLevel] = useState(500);
+  const [format, setFormat] = useState("hex");
 
   const colorBoxes = palette.colors[level].map((item) => (
-    <ColorBox background={item.hex} name={item.name} key={item.name} />
+    <ColorBox background={item[format]} name={item.name} key={item.name} />
   ));
 
   return (
     <div className="Palette">
-      <Navbar level={level} handleChange={(newLevel) => setLevel(newLevel)} />
+      <Navbar
+        level={level}
+        onLevelChange={(newLevel) => setLevel(newLevel)}
+        format={format}
+        onFormatChange={(e) => {
+          setFormat(e.target.value);
+        }}
+      />
       <div className="Palette-colors">{colorBoxes}</div>
     </div>
   );
